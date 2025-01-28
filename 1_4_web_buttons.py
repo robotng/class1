ac53361f6592af832f6a29e84ec2b603a01ac283
TNG Robotics <tngrobot@gmail.com>
2025-01-27 21:14:13 -0400

    Update 1_4_web_buttons.py
    Improved to allow multiple people to connect to the same IP, for example, 192.168.0.198 on the same Wi-Fi network. This mean multiple people can control the same LED now, instead of the previous code, which queued up people one at a time to access the website. This new code will allow the WeMos D1 Mini running as a server to handle about 4-8 simultaneous users toggling the LED, each making a single request at a time.
    
    Pin2 changed to Pin4, to avoid bull issues where Pin2 needs to be kept pulled high at bootload of WeMos.

--- 1_4_web_buttons.py
+++ 1_4_web_buttons.py
@@ -1,42 +1,69 @@
 from machine import Pin
 import network
 import socket
+import select
 
 # LED setup
-led = Pin(2, Pin.OUT)
+led = Pin(4, Pin.OUT)
 
 # Wi-Fi setup
-ssid = 'Your-WIFI'
-password = 'Your-PASSWORD'
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-wlan.connect(ssid, password)
-while not wlan.isconnected():
-    pass
-print('Wi-Fi connected, see website at:', wlan.ifconfig()[0])
+ssid = 'Your-WiFi'
+password = 'Your-Password'
 
-# HTML website code
+def connect_wifi():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(ssid, password)
+    while not wlan.isconnected():
+        pass
+    print('Wi-Fi connected, see website at:', wlan.ifconfig()[0])
+
+# HTML page
 html = """
 <!DOCTYPE html>
 <html>
 <body style='text-align:center;'>
     <h1>Toggle LED</h1>
-    <form action='/on'><button>Turn ON</button></form>
-    <form action='/off'><button>Turn OFF</button></form>
+    <form action='/on'><button><h2>Turn ON</h2></button></form>
+    <form action='/off'><button><h2>Turn OFF</h2></button></form>
 </body>
 </html>
 """
 
-# Start web server
-server = socket.socket()
-server.bind(('', 80))
-server.listen(1)
-print('Server running...')
+# Web server
+def start_server():
+    server = socket.socket()
+    server.bind(('', 80))
+    server.listen(5)
+    server.setblocking(False)
+    print('Server running...')
+    return server
 
-# Handle web requests
+def handle_client(s):
+    try:
+        request = s.recv(1024).decode()
+        if '/on' in request:
+            led.on()
+        elif '/off' in request:
+            led.off()
+        s.send("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n" + html)
+    except Exception as e:
+        print(f'Error: {e}')
+    finally:
+        s.close()
+
+# Main loop
+connect_wifi()
+server = start_server()
+
+connections = [server]
 while True:
-    conn, _ = server.accept()
-    request = conn.recv(1024).decode()
-    led.on() if '/on' in request else led.off() if '/off' in request else None
-    conn.send("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n" + html)
-    conn.close()
+    readable, _, _ = select.select(connections, [], [])
+    for s in readable:
+        if s is server:
+            conn, _ = server.accept()
+            conn.setblocking(False)
+            connections.append(conn)
+        else:
+            handle_client(s)
+            connections.remove(s)
